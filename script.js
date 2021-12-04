287a45d64cc80cd3f7f3033eda76c4590092f5cf
Rafael Cavalcante <rafael.cavalcante.es@gmail.com>
2021-12-04 16:59:57 -0300

    cria botao e logica para editar uma linha da tabela

--- script.js
+++ script.js
@@ -8,14 +8,97 @@ function clearInputs() {
   document.getElementById('age').value = '';
 }
 
+function createEditInputs (e) {
+  const row = e.target.parentElement;
+  const name = row.firstChild;
+  const age = name.nextSibling;
+
+  const nameInput = document.createElement('input');
+  nameInput.value = name.innerText;
+
+  const ageInput = document.createElement('input')
+  ageInput.value = age.innerText;
+
+
+  
+  name.appendChild(nameInput);
+  age.appendChild(ageInput);
+}
+
+function saveEdit(e) {
+  const row = e.target.parentElement;
+  const name = row.firstChild;
+  const age = name.nextSibling;
+  const saveButton = age.nextSibling;
+  const cancelButton = saveButton.nextSibling;
+  const nameInput = name.firstElementChild;
+  const ageInput = age.firstElementChild;
+
+  personArray.forEach((person) => {
+    if(person.name === name.innerText){
+      person.name = nameInput.value;
+      person.age = ageInput.value;
+    }
+  })
+
+  name.innerText = nameInput.value;
+  age.innerText = ageInput.value;
+
+  // name.removeChild(nameInput);
+  // age.removeChild(ageInput);
+
+  const editButton = document.createElement('button');
+  editButton.innerHTML = 'editar';
+  editButton.addEventListener('click', editPerson);
+  row.insertBefore(editButton, saveButton);
+
+  row.removeChild(saveButton);
+  row.removeChild(cancelButton);
+
+  orderTable();
+}
+
+function cancelEdit(e) {
+  const row = e.target.parentElement;
+  const name = row.firstChild.innerText;
+  const age = row.firstChild.nextSibling.innerText;
+  const newRow = createRow(name, age);
+  row.parentElement.replaceChild(newRow, row);
+}
+
+function createEditButtons(e) {
+  const editButton = e.target;
+
+  const saveButton = document.createElement('button');
+  saveButton.innerHTML = 'salvar';
+  saveButton.addEventListener('click', saveEdit);
+
+  const cancelButton = document.createElement('button');
+  cancelButton.innerHTML = 'cancelar';
+  cancelButton.addEventListener('click', cancelEdit);
+
+
+  editButton.parentElement.insertBefore(saveButton, editButton);
+  editButton.parentElement.insertBefore(cancelButton, editButton);
+  editButton.parentElement.removeChild(editButton);
+}
+
+function editPerson(e) {
+  createEditInputs(e);
+  createEditButtons(e);
+
+}
+
 function deletePerson(e) {
   if(!window.confirm('Deseja apagar esta linha?')) return;
 
-  const name = e.target.parentElement.firstChild.innerText;
+  const row = e.target.parentElement;
+  const table = row.parentElement
+
+  const name = row.firstChild.innerText;
   personArray = personArray.filter((person) => person.name !== name);
 
-  const table = e.target.parentElement.parentElement
-  const row = e.target.parentElement;
+  
   table.removeChild(row);
 }
 
@@ -24,10 +107,8 @@ function orderTable() {
     return (a.age > b.age) ? -1 : ((b.age > a.age) ? 1 : 0);
   });
   Array.from(personTable.children).forEach((child, index) => {
-    if(index != 0) {
-    child.firstChild.innerText = personArray[index - 1].name;
-    child.firstChild.nextSibling.innerText = personArray[index - 1].age;
-    }
+    child.firstChild.innerText = personArray[index].name;
+    child.firstChild.nextSibling.innerText = personArray[index].age;
   });
  
 }
@@ -46,6 +127,11 @@ function createRow(name, age) {
   const ageColumn = document.createElement('td');
   ageColumn.innerText = age;
   row.appendChild(ageColumn);
+
+  const editButton = document.createElement('button');
+  editButton.innerHTML = 'editar';
+  editButton.addEventListener('click', editPerson);
+  row.appendChild(editButton);
 
   const deleteButton = document.createElement('button');
   deleteButton.innerHTML = 'X';
